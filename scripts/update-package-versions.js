5cee113673d706b87ff37a0b88126da8f617eb47
Jeff Manke <jeffman879@gmail.com>
2023-06-15 13:31:37 -0700

    init

---
+++ scripts/update-package-versions.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const lockPath = "yarn.lock";
+
+function deletePackageEntries(patterns) {
+  let data = fs.readFileSync(lockPath, "utf8");
+
+  patterns.forEach((pattern) => {
+    data = data.replace(pattern, "");
+  });
+
+  fs.writeFileSync(lockPath, data, "utf8");
+}
+
+const patterns = [
+  /"@esri\/calcite-components@npm:.+?":.+?linkType: (soft|hard)"/s,
+];
+
+deletePackageEntries(patterns);
